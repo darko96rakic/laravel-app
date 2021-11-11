3b4493f0764a17963a4493a1be970d42bf491728
darko96rakic <rakicdarko96@gmail.com>
2021-11-11 18:42:15 +0100

    feat: added route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiResponseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('getMostPopularVideos',[ApiResponseController::class,'getMostPopularVideos']);
