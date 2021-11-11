5c9a94817f5d2594723a85dd581b89f41a5f15ab
darko96rakic <rakicdarko96@gmail.com>
2021-11-11 19:40:39 +0100

    fix: imported classes

--- app/Http/Resources/MostPopularVideosResource.php
+++ app/Http/Resources/MostPopularVideosResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class MostPopularVideosResource extends JsonResource
@@ -9,15 +10,15 @@ class MostPopularVideosResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param  Request  $request
+     * @return array
      */
-    public function toArray($request)
+    public function toArray($request) : array
     {
         return [
-          'countryName' => $this['countryName'],
-           'foreword' => $this['foreword'],
-           'videos' => YouTubeVideoResource::collection($this['videos'])
+            'countryName' => $this['countryName'],
+            'foreword' => $this['foreword'],
+            'videos' => YouTubeVideoResource::collection($this['videos'])
         ];
     }
 }
